4d2e64709e36fb9102c5d3f7d9e991d9e7901f72
princy1103 <princyjoshi5670@gmail.com>
2025-09-11 16:38:28 +0530

    Updated Blog

--- Final Exam/src/store/auth/reducer.js
+++ Final Exam/src/store/auth/reducer.js
@@ -1,19 +1,25 @@
 import { SIGN_IN, SIGN_OUT } from './actions';
 
+
+const savedUser = JSON.parse(localStorage.getItem("user"));
+const savedAuth = !!savedUser; // true if user exists
+
 const initialState = {
-  isAuthenticated: false,
-  user: null,
+  isAuthenticated: savedAuth,
+  user: savedUser,
 };
 
 export function authReducer(state = initialState, action) {
   switch (action.type) {
     case SIGN_IN:
+      localStorage.setItem("user", JSON.stringify(action.payload)); 
       return { ...state, isAuthenticated: true, user: action.payload };
+
     case SIGN_OUT:
+      localStorage.removeItem("user");
       return { ...state, isAuthenticated: false, user: null };
+
     default:
       return state;
   }
 }
-
-
